9ca16718b8239e66c169a4d9b6f91614e585f3f5
jinoj <jinojoe@gmail.com>
2018-04-13 10:22:06 +0530

    Update find_googleauth_file.sh

--- find_googleauth_file.sh
+++ find_googleauth_file.sh
@@ -4,3 +4,15 @@ for i in ${user_list[@]}
 do
 	find /home/$i/.google_authenticator -exec cp {} /destination/data/ \;
 done
+
+
+
+=====================================================================================\\
+try this one
+
+#!/bin/bash
+user_list=($(awk -F ":" '$3 > 499' /etc/passwd | cut -d ':' -f6))
+for i in ${user_list[@]}
+do
+	find $i/.google_authenticator -exec cp {} /destination/data/ \;
+done
